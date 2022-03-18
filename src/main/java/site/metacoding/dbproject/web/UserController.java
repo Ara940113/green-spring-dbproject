0deb4943d4ece76d8faca75b4a9be8bc620425bd
AR <aki1013@naver.com>
2022-03-18 09:02:14 +0900

    19.회원정보 수정 - 더티체킹 사용

--- src/main/java/site/metacoding/dbproject/web/UserController.java
+++ src/main/java/site/metacoding/dbproject/web/UserController.java
@@ -1,7 +1,5 @@
 package site.metacoding.dbproject.web;
 
-import java.util.Optional;
-
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -13,11 +11,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import lombok.RequiredArgsConstructor;
 import site.metacoding.dbproject.domain.user.User;
-import site.metacoding.dbproject.domain.user.UserRepository;
 import site.metacoding.dbproject.service.UserService;
 import site.metacoding.dbproject.web.dto.ResponseDto;
 
@@ -27,18 +25,14 @@ public class UserController {
 
     // 컴퍼지션 (의존성 연결)
     private final UserService userService;
-    private HttpSession session;
+    private final HttpSession session;
 
-    // DI 받는 코드!!
-
-    // http://localhost:8000/api/user/username/same-check?username=s
-    // user의 username이 동일한지 확인해줄래? -응답(json)
-    @GetMapping("/api/user/username/same-check") // 예외적으로 동사 허용
+    // http://localhost:8080/api/user/username/same-check?username=s
+    // user의 username이 동일한지 확인해줄래? - 응답 (무조건 Json)
+    @GetMapping("/api/user/username/same-check")
     public @ResponseBody ResponseDto<String> sameCheck(String username) {
-
         String data = userService.유저네임중복검사(username);
-        return new ResponseDto<String>(1, "통신성공", data);
-
+        return new ResponseDto<String>(1, "통신 성공", data);
     }
 
     // 회원가입 페이지 (정적) - 로그인X
@@ -50,12 +44,12 @@ public class UserController {
     // username=ssar&password=&email=ssar@nate.com 패스워드 공백
     // username=ssar&email=ssar@nate.com 패스워드 null
     // username=ssar&password=1234&email=ssar@nate.com (x-www-form)
-    // 회원가입 - 로그인X
+    // 회원가입 (페이지가 아니라 액션을 주는 것 - 회원가입 수행) - 로그인X
     @PostMapping("/join")
     public String join(User user) {
 
         // 필터의 역할
-        // 1. username, password, email 1.null체크, 2.공백체크
+        // 1. username, password, email - 1. null체크, 2. 공백체크
         if (user.getUsername() == null || user.getPassword() == null || user.getEmail() == null) {
             return "redirect:/joinForm";
         }
@@ -64,14 +58,13 @@ public class UserController {
         }
 
         userService.회원가입(user);
-
-        return "redirect:/loginForm"; // 로그인페이지 이동해주는 컨트롤러 메서드를 재활용
+        return "redirect:/loginForm"; // 로그인 페이지 이동해주는 컨트롤러 메서드 재활용
     }
 
     // 로그인 페이지 (정적) - 로그인X
     @GetMapping("/loginForm")
     public String loginForm(HttpServletRequest request, Model model) {
-        // jSessionId=fjsdklfjsadkfjsdlkj333333;remember=ssar
+        // JSESSIONID=asidaisdjasdi1233;remember=ssar
         // request.getHeader("Cookie");
         if (request.getCookies() != null) {
             Cookie[] cookies = request.getCookies(); // jSessionId, remember 두개가 있음.
@@ -87,30 +80,30 @@ public class UserController {
     }
 
     // SELECT * FROM user WHERE username=? AND password=?
-    // 원래 SELECT 는 무조건 get요청
+    // 원래 SELECT는 무조건 get 요청
     // 그런데 로그인만 예외 (POST)
-    // 이유 : 주소에 패스워드를 남길 수 없으니까!!
-    // 로그인 - - 로그인X
+    // 이유 : 주소에 패스워드를 남길 수 없으니까!!!
+    // 로그인 수행 - 로그인X
     @PostMapping("/login")
     public String login(User user, HttpServletResponse response) {
+
         User userEntity = userService.로그인(user);
 
         if (userEntity != null) {
-            session.setAttribute("principal", userEntity);
+            session.setAttribute("principal", userEntity); // 세션에 등록
             if (user.getRemember() != null && user.getRemember().equals("on")) {
                 response.addHeader("Set-Cookie", "remember=" + user.getUsername());
-            }
+            } // remember me
             return "redirect:/";
         } else {
             return "redirect:/loginForm";
         }
-
     }
 
     // 로그아웃 - 로그인O
     @GetMapping("/logout")
     public String logout() {
-        session.invalidate();
+        session.invalidate(); // 영역 전체를 날리는 것
         return "redirect:/loginForm"; // PostController 만들고 수정하자.
     }
 
@@ -119,16 +112,15 @@ public class UserController {
     @GetMapping("/s/user/{id}")
     public String detail(@PathVariable Integer id, Model model) {
 
-        // 유효성 검사 하기 (수십개....엄청 많겠죠?)
-
+        // 유효성 검사하기 (수십개...엄청 많음)
         User principal = (User) session.getAttribute("principal");
 
-        // 1. 인증 체크 -컨트롤러권한
-        if (principal == null) {
-            return "error/page1";
+        // 1. 인증 체크
+        if (principal == null) { // 로그인 인증해야 접속 가능하게 하기
+            return "error/page1"; // 못 가게 해야됨
         }
 
-        // 2. 권한체크 -컨트롤러권한
+        // 2. 권한 체크
         if (principal.getId() != id) {
             return "error/page1";
         }
@@ -149,11 +141,34 @@ public class UserController {
         return "user/updateForm";
     }
 
+    // username(X), password(O), email(O)
+    // password=1234&email=ssar@nate.com (x-www-form-urlencoded)
+    // { "password" : "1234", "email" : "ssar@nate.com" } (application/json)
+    // json을 받을 것이기 때문에 Spring이 데이터 받을 때 파싱전략을 변경해줘야 한다!!
+    // Put 요청은 Http Body가 있다. Http Header의 Content-Type에 MIME 타입을 알려줘야 한다.
+
+    // @RequestBody -> BufferedReader + JSON 파싱(자바 오브젝트)
+    // @ResponseBody -> BufferedWritier + JSON 파싱(자바 오브젝트)
+
     // 유저수정 - 로그인O
     @PutMapping("/s/user/{id}")
-    public String update(@PathVariable Integer id) {
+    public @ResponseBody ResponseDto<String> update(@PathVariable Integer id, @RequestBody User user) {
 
-        return "redirect:/user/" + id;
-    }
+        // 유효성 검사하기 (수십개...엄청 많음)
+        User principal = (User) session.getAttribute("principal");
 
+        // 1. 인증 체크
+        if (principal == null) { 
+            return new ResponseDto<String>(-1,"인증안됨",null);
+        }
+
+        // 2. 권한 체크
+        if (principal.getId() != id) {
+            return new ResponseDto<String>(-1,"권한없어",null);
+        }
+
+        User userEntity = userService.유저수정(id, user);
+        session.setAttribute("principal", userEntity); // 세션변경 - 덮어쓰기
+
+        return new ResponseDto<String>(1,"성공",null);
 }
